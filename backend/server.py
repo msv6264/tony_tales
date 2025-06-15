4d5da8abdd2a9682a1b6bf42d3813747b146a4bc
Sri Vaishnavi <srivaishnavi0602@gmail.com>
2025-06-15 15:04:32 +0530

    Integrated Vite build with Flask and added static files

--- backend/server.py
+++ backend/server.py
@@ -1,19 +1,19 @@
 import cohere
 import secrets
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from dotenv import load_dotenv 
 import os
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 app.secret_key = secrets.token_hex(16)
 
 key = os.getenv('COHERE_API_KEY')
 co = cohere.Client(key)
 
-CORS(app, origins=["http://localhost:5173"], supports_credentials=True)
+CORS(app, origins="*", supports_credentials=True)
 
 mood_prompts = {
     "smile": "Write a joyful story. Give me the whole story without asking me any questions.",
@@ -38,6 +38,11 @@ def sending():
     input_prompt = mood_prompts.get(emoji, "Write a story.")
     story = generate_story(input_prompt)
     return jsonify({"Story": story})
+
+# to use frontend in templates and stativ folders
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
